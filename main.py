487981803e6afbde307df2bff7ac3625a4c46510
Larry Nesbit <larryjnesbit@gmail.com>
2017-01-20 17:30:02 -0600

    added fortune, lucky number, and refressh button

--- main.py
+++ main.py
@@ -15,11 +15,37 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+
+def getRandomFortune():
+
+    fortunes = [
+    "i see much code in your future",
+    "consider eating more cookies",
+    "you have tamed the mighty Python, now you must free it onto the Great Spider's Web"
+    ]
+
+    index = random.randint(0,2)
+
+    return fortunes[index]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello  my world!')
+        header = "<h1>Fortune Cookie</h1>"
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+        fortune = "<strong>" + getRandomFortune() + "</strong>"
+        fortune_sentence = "Your Fortune: " + fortune
+        fortune_paragraph ="<p>" + fortune_sentence + "</p>"
+
+        lucky_number= "<strong>" + str(random.randint (1,100)) + "</strong>"
+        number_sentence='Your lucky number: '+ (lucky_number)
+        number_paragraph="<p>" + number_sentence + "</p>"
+
+        cookie_again_button = "<a href= '.'><button>another cookie plz!</button></a>"
+        content = header +  fortune_paragraph + number_paragraph + cookie_again_button
+        self.response.write(content)
+
+app = webapp2.WSGIApplication( [
+('/', MainHandler)],
+debug=True)
